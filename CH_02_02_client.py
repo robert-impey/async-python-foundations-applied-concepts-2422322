593ec0216a149faa35656a7c15b2fdc59e98efbd
Robert Impey <robert-impey@users.noreply.github.com>
2025-02-13 23:16:52 +0000

    Host name

--- CH_02_02_client.py
+++ CH_02_02_client.py
@@ -13,7 +13,7 @@ def render_response(message):
 
 
 async def astronout():
-    uri = "ws://localhost:8765"
+    uri = "ws://fatima.robertimpey.com:8765"
     async with websockets.connect(uri) as websocket:
         is_ready = input("Are you ready? ")
 
